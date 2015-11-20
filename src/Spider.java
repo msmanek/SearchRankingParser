7563b5824a82ec08244b36331023e307f0bf017c
MohammedShabbar Manek <m.manek@mail.utoronto.ca>
2015-11-19 23:18:05 -0500

    Finish parser as per specs on netinstructions tutorial

--- src/Spider.java
+++ src/Spider.java
@@ -1,19 +1,68 @@
-import java.util.LinkedList;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
 public class Spider {
-	private static final int _MAX_PAGES_TO_SEARCH_ = 3;
-	private HashSet<String> pagesVisited = new HashSet<String>();
-    private LinkedList<String> pagesToVisit = new LinkedList<String>();
-        
-    private String nextUrl() {
+	private static final int MAX_PAGES_TO_SEARCH = 10;
+	private Set<String> pagesVisited = new HashSet<String>();
+	private List<String> pagesToVisit = new LinkedList<String>();
+
+	/**
+	 * Our main launching point for the Spider's functionality. Internally it
+	 * creates spider legs that make an HTTP request and parse the response (the
+	 * web page).
+	 * 
+	 * @param url
+	 *            - The starting point of the spider
+	 * @param searchWord
+	 *            - The word or string that you are searching for
+	 */
+	public void search(String url, String searchWord) {
+
+		while (this.pagesVisited.size() < MAX_PAGES_TO_SEARCH) {
+
+			String currentUrl;
+			SpiderLeg leg = new SpiderLeg();
+
+			if (this.pagesToVisit.isEmpty()) {
+				currentUrl = url;
+				this.pagesVisited.add(url);
+			}// end if
+			else
+				currentUrl = this.nextUrl();
+
+			leg.crawl(currentUrl); // Look at the crawl method in SpiderLeg
+
+			boolean success = leg.searchForWord(searchWord);
+			if (success) {
+				System.out.println(String.format(
+						"**Success** Word %s found at %s", searchWord,
+						currentUrl));
+				break;
+			}
+
+			this.pagesToVisit.addAll(leg.getLinks());
+		}
+		System.out.println("\n**Done** Visited " + this.pagesVisited.size()
+				+ " web page(s)");
+	}
+
+	/**
+	 * Returns the next URL to visit (in the order that they were found). We
+	 * also do a check to make sure this method doesn't return a URL that has
+	 * already been visited.
+	 * 
+	 * @return
+	 */
+	private String nextUrl() {
 		String nextUrl;
-		do{
-		    nextUrl = this.pagesToVisit.remove(0);
-		} while(this.pagesVisited.contains(nextUrl));
-		
+		do {
+			nextUrl = this.pagesToVisit.remove(0);
+		} while (this.pagesVisited.contains(nextUrl));
 		this.pagesVisited.add(nextUrl);
-		
 		return nextUrl;
-    }//end nextUrL function
-}//end Spider class
+	}// end nextUrl method
+
+}// end Spider class
+
