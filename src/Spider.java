93c80935b0757e4e7a8115639e5a249fa525a7b3
MohammedShabbar Manek <m.manek@mail.utoronto.ca>
2015-11-19 22:27:28 -0500

    Add template for SpiderLeg class

--- src/Spider.java
+++ src/Spider.java
@@ -1,24 +1,19 @@
-import java.awt.List;
 import java.util.LinkedList;
-import java.util.Set;
 import java.util.HashSet;
-import java.io.*;
-import jsoup-1.8.3.jar;
 
 public class Spider {
 	private static final int _MAX_PAGES_TO_SEARCH_ = 3;
-	private Set<String> pagesVisited = new HashSet<String>();
-    private List<String> pagesToVisit = new LinkedList<String>();
-    
-    public static void main(String[] args) {
-    	System.out.print("Hello World");
-    }
-    
-    private String nextURL() {
-    	String nextUrl;
-    	
-    	do {
-    		
-    	} while(this.pagesVisited.contains(nextUrl));
-    }
-}
+	private HashSet<String> pagesVisited = new HashSet<String>();
+    private LinkedList<String> pagesToVisit = new LinkedList<String>();
+        
+    private String nextUrl() {
+		String nextUrl;
+		do{
+		    nextUrl = this.pagesToVisit.remove(0);
+		} while(this.pagesVisited.contains(nextUrl));
+		
+		this.pagesVisited.add(nextUrl);
+		
+		return nextUrl;
+    }//end nextUrL function
+}//end Spider class
---
+++ src/SpiderLeg.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class SpiderLeg {
+	
+	// Give it a URL and it makes an HTTP request for a web page
+	public void crawl(String nextURL) {}
+	
+	// Tries to find a word on the page
+	public boolean searchForWord(String word) {}
+	
+	// Returns a list of all the URLs on the page
+	public LinkedList<String> getLinks() {}
+	
+}//end SpiderLeg class
